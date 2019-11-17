61f654e3db09bfd1a946c903a179ac1e6dddadc4
Ginestra <alex.ginestra4@etu.univ-lorraine.fr>
2019-11-17 19:27:06 +0100

    creation crud beer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CrudBearModule } from './crud-bear/crud-bear.module';
 
 @Module({
-  imports: [],
+  imports: [CrudBearModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/crud-beer/crud-beer.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('crud-beer')
+export class CrudBeerController {}
