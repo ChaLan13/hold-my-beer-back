3fd29494a71da7fdd67bc0ce0aa118ced991a404
Ginestra <alex.ginestra4@etu.univ-lorraine.fr>
2019-11-18 23:27:04 +0100

    script bd

--- DB/mongo-init.js
+++ DB/mongo-init.js
@@ -1,12 +1,35 @@
-db.createUser(
+db.getCollection('beer').insertMany([
     {
-        user : "admin",
-        pwd : "admin",
-        role : [
-            {
-                role : "readWritte",
-                db : "HMB_DB"
-            }
-        ]
-    }
-)
+        "id": "5763cd4d9d2a4f259b53c901",
+        "name": "Pilsner",
+        "country": "Germany",
+        "cereal": [ "oat", "hop" ],
+        "birthYear": "1984",
+        "shop": {
+            "priceALiter": NumberDecimal(3.50),
+            "URL": "http://www.monsite.com"
+        }
+    },
+    {
+        "id": "5763cd4d51fdb6588742f99e",
+        "name": "Guinness Classic",
+        "country": "Ireland",
+        "cereal": [ "hop" ],
+        "birthYear": "1967",
+        "shop": {
+            "priceALiter": NumberDecimal(2.50),
+            "URL": "http://www.monsite.com"
+        }
+    },
+    {
+        "id": "5763cd4dba6362a3f92c954e",
+        "name": "Delirium",
+        "country": "Ireland",
+        "cereal": [ "malt" ],
+        "birthYear": "1947",
+        "shop": {
+            "priceALiter": NumberDecimal(4.50),
+            "URL": "http://www.monsite.com"
+        }
+    },
+])
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {CrudBeerModule} from './CRUD/crud-beer.module';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import * as Config from 'config';
 
 @Module({
-  imports: [ CrudBeerModule ],
+  imports: [ CrudBeerModule,
+    MongooseModule.forRoot(Config.get<string>('mongodb.uri'), Config.get<MongooseModuleOptions>('mongodb.options')),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
